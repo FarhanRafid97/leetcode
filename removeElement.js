c1d33121430992d11f2016a0648c892609615cdb
farhan rafid syauqi <farhanrafid97@gmail.com>
2023-06-05 15:26:31 +0700

    removeElement solution

--- removeElement.js
+++ removeElement.js
@@ -1,11 +1,12 @@
 var removeElement = function (nums, val) {
-  let k = [];
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== val) {
-      k.push(nums[i]);
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+
+      i -= 1;
     }
   }
 
-  return k;
+  return nums.length;
 };
-console.log(removeElement([3, 2, 2, 3], (val = 3)));
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], (val = 2)));

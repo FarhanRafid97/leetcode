840bbc21a6f6940565811558c28e912d4c525da0
farhan rafid syauqi <farhanrafid97@gmail.com>
2023-06-16 10:32:42 +0700

    solution

---
+++ isIsomorphic.js
@@ -0,0 +1,11 @@
+var isIsomorphic = function (s, t) {
+  for (let i = 0; i < s.length; i++) {
+    console.log(s.indexOf(s[i], i + 1));
+    if (s.indexOf(s[i], i + 1) !== t.indexOf(t[i], i + 1)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+isIsomorphic('bbbaaaba', 'aaabbbba');

b25d93450c51b039224431c63c14fc818565fef3
farhan rafid syauqi <farhanrafid97@gmail.com>
2023-06-16 10:35:01 +0700

    solution restorestring

---
+++ restoreString.js
@@ -0,0 +1,6 @@
+var restoreString = function (s, indices) {
+  let output = Array(indices.length - 1);
+  const str = output.map((d, i) => indices[i]);
+};
+
+restoreString('codeleet', [4, 5, 6, 7, 0, 2, 1, 3]);
